68b5d18863d62d3cf7b816e263416faec0a97ac3
Alexander Jacks <alexanderthejacks@gmail.com>
2019-10-03 09:25:58 -0700

    :bug: form submission feature

--- src/pages/login.js
+++ src/pages/login.js
@@ -10,29 +10,30 @@ import { graphql } from 'gatsby';
 const Login = ({ data }) =>  (
     <Layout>
         <Box>
-            <Title>Schedule a consultation</Title>
-            <form name="contact" method="POST" data-netlify="true">
-				<p>
-					<label>Your Name: <input type="text" name="name" /></label>   
-				</p>
-				<p>
-					<label>Your Email or SMS: <input type="email" name="email" /></label>
-				</p>
-				<p>
-					<label>Goal: <select name="role[]" multiple>
-						<option value="leader">Learn More, Initial Consult</option>
-						<option value="follower">Fix / Enhance Existing Website</option>
-						<option value="leader">New Website, App</option>
-					</select>
-					</label>
-				</p>
-				<p>
-					<label>Message: <textarea name="message"></textarea></label>
-				</p>
-				<p>
-					<button type="submit">Send Request</button>
-				</p>
-			</form>
+          <Title>Schedule a consultation</Title>
+        {/* form handling at runtime via netlify */}
+          <form name="consult-contact-me" method="POST" data-netlify="true">
+    				<p>
+    					<label>Your Name: <input type="text" name="name" /></label>   
+    				</p>
+    				<p>
+    					<label>Your Email or SMS: <input type="email" name="email" /></label>
+    				</p>
+    				<p>
+    					<label>Your Goal: <select name="role[]" multiple>
+    						<option value="leader">Learn More, Initial Consult</option>
+    						<option value="follower">Fix / Enhance Existing Website</option>
+    						<option value="leader">New Website, App</option>
+    					</select>
+    					</label>
+    				</p>
+    				<p>
+    					<label>Brief Message (emojis welcome <span>✌️</span>): <textarea name="message"></textarea></label>
+    				</p>
+    				<p>
+    					<button type="submit">Send Request</button>
+    				</p>
+    			</form>
         </Box>
     </Layout>
 );
