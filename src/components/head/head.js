e2a70559f02c2eb0e145230a0c464819b905ff81
Alexander Jacks <alexanderthejacks@gmail.com>
2019-10-27 11:47:52 -0700

    :wrench: adds AdSense affiliate script

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -150,6 +150,11 @@ const Head = ({
       type="image/png"
     />
 
+{/*
+  begin AdSense
+  */}
+    <script data-ad-client="ca-pub-1699472970547311" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
     <script type="application/ld+json">
       {JSON.stringify(
         schemaGenerator({
