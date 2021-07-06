d0dd5a72e9b92151c104d16d22a547d7d2263dd9
jacksmedia <alex@j4cks.com>
2021-07-06 11:29:53 -0700

    :art: nicer 2020s gradient footer, header

--- src/components/footer/footer.css.js
+++ src/components/footer/footer.css.js
@@ -8,8 +8,8 @@ export const Container = styled.header`
     text-shadow: 1px 1px 2px black, 0 0 1em #1d0200, 0 0 0.2em #1d0200;
     padding: 2vw;
   }
-  background-image: linear-gradient(#4c9084, #8fb67b, #4c9084);
   max-width: 100vw;
+  padding: 3rem 0rem 0rem;
   display: flex;
   flex-flow: column nowrap;
   justify-content: space-around;
@@ -24,4 +24,7 @@ export const Container = styled.header`
       color: inherit;
     }
   }
+  background: -webkit-linear-gradient(270deg, rgba(221,60,0,0.8), rgba(45,0,0,0) 66.6%),-webkit-linear-gradient(0deg, rgba(0,153,199,0.8), rgba(0,45,0,0) 66.6%),-webkit-linear-gradient(90deg, rgba(5,0,255,0.8), rgba(0,0,45,0) 66.6%);
+  background: linear-gradient(270deg, rgba(221,60,0,0.8), rgba(45,0,0,0) 66.6%),linear-gradient(0deg, rgba(0,153,199,0.8), rgba(0,45,0,0) 66.6%),linear-gradient(90deg, rgba(5,0,255,0.8), rgba(0,0,45,0) 66.6%);
 `;
+  
